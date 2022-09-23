6c716d47aff9036e5c9abc7d2fa8169b3514b6e8
Vinicius Silva <ferreiravini468@gmail.com>
2022-09-23 20:56:34 -0300

    creating all api routes

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,23 @@
 const router = require('express').Router()
 
+router.get('/task/all', (req, res) => {
+    res.send(`all tasks`)
+})
+
+router.post('/task/store', (req, res) => {
+    res.send('storing task...')
+})
+
+router.put('/task/edit/:id', (req, res) => {
+    res.send(`editing task ${req.params.id}`)
+})
+
+router.delete('/task/remove/:id', (req, res) => {
+    res.send(`removing task ${req.params.id}`)
+})
+
+router.get('/task/:id', (req, res) => {
+    res.send(`task ${req.params.id}`)
+})
+
 module.exports = router
