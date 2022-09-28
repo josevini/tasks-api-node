984022378d239e341e896fae74b3b0273c6989e7
Vinicius Silva <ferreiravini468@gmail.com>
2022-09-28 20:08:32 -0300

    setting body-parser

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const app = express()
 require('dotenv').config()
 const env = process.env
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 const routes = require('./routes')
 app.use(routes)
 
